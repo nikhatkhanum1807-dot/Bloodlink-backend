e9a5ea395f9012d4fe370d3c15ce9cd9f247ea14
nikhatkhanum1807-dot <nikhatkhanum1807@gmail.com>
2026-06-09 22:10:17 +0530

    fix CORS for production

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
